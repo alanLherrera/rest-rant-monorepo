d308a0f1319aff94f8cb1f478ba4ddcfbdd1293e
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-02-16 19:39:43 -0800

    jwt version

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 export const CurrentUser = createContext()
-
-function CurrentUserProvider({ children }){
+    
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
 
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5001/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 }
+
 
 export default CurrentUserProvider
